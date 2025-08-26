f725dc790163e17afd5fb33dea6135343b30df0d
rasukutty <rasukutty1098@gmail.com>
2025-08-26 19:41:41 +0530

    fix: hidden first reponse

--- desk/src/components/ticket/TicketAgentDetails.vue
+++ desk/src/components/ticket/TicketAgentDetails.vue
@@ -1,28 +1,4 @@
-<template>
-  <div class="flex flex-col gap-3 border-b px-6 py-3">
-    <div
-      v-for="s in sections"
-      :key="s.label"
-      class="flex items-center text-base leading-5"
-    >
-      <Tooltip :text="s.label">
-        <div class="w-[126px] text-sm text-gray-600">{{ s.label }}</div>
-      </Tooltip>
-      <div class="flex items-center justify-between">
-        <div v-if="s.value">{{ s.value }}</div>
-        <Tooltip :text="s.tooltipValue">
-          <Badge
-            v-if="s.badgeText"
-            class="-ml-1"
-            :label="s.badgeText"
-            variant="subtle"
-            :theme="s.badgeColor"
-          />
-        </Tooltip>
-      </div>
-    </div>
-  </div>
-</template>
+
 
 <script setup lang="ts">
 import { dayjs } from "@/dayjs";
